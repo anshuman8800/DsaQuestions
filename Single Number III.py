9cb40eb349981bffd8d8fd141df4213352354122
2107shantanu <54627896+2107shantanu@users.noreply.github.com>
2022-10-06 22:57:24 +0530

    Create Single Number III.py

---
+++ Single Number III.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor =0
+        for n in nums:
+            xor = xor^n
+        firstbit = xor & (xor-1) ^ xor
+        num1 = 0
+        for n in nums:
+            if n & firstbit:
+                num1 = num1^n
+        return [num1,num1^xor]        
+        
